1e29b2b56a35274d1ffb654783beb1fb6f8b1896
Ilona <mitsunaki@yandex.ru>
2022-05-21 15:56:03 +0300

    feat: complete files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.resolve(__dirname, '\secret-folder');
+
+async function readDir(folder) {
+    let files = await fs.promises.readdir(folder);
+    await files.forEach(file => {
+        fs.stat(path.join(folder, file), (err, stats) => {
+            if (err) {
+                throw err;
+            }
+            if (stats.isFile()) {
+                console.log (`${path.basename(file, path.extname(file))} - ${path.extname(file).slice(1)} - ${stats.size}b`);
+            }
+        })
+    })
+};
+
+readDir(folderPath);
+
