3813df374fd5834e49e08ad5ed3655a17f0d145b
Ilona <mitsunaki@yandex.ru>
2022-05-21 17:54:09 +0300

    feat: complete copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const {stdout, stdin} = process;
 
+const newFolderPath = path.join(__dirname, 'files-copy');
+const folderPath = path.join(__dirname, 'files');
+
+async function copyDir(from, to) {
+    await fs.promises.rm(to, {recursive:true, force:true});
+    await fs.promises.mkdir(to, {recursive:true}, stdout.write('Копия папки с файлами создана!'));
+    let files = await fs.promises.readdir(from);
+    await files.forEach(file => {
+        fs.promises.copyFile(path.join(from, file), path.join(to,file));
+    })
+}
+
+copyDir(folderPath, newFolderPath);
